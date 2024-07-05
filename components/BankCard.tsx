91de3347cf75513337938da87d848b4eb2f327bc
Samlarrytech <samlarrytech@yandex.com>
2024-07-05 06:10:45 +0300

    finalize the app

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,16 +2,17 @@ import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Copy from './Copy'
 
 const BankCard = ({account, userName, showBalance 
   = true}: CreditCardProps) => {
   return (
     <div className='flex flex-col'>
-      <Link href="/" className='bank-card'>
+      <Link href={`/transaction-history/?id=${account.appwriteItemId}`} className="bank-card">
         <div className='bank-card_content'>
           <div>
             <h1 className='text-16 font-semibold text-white'>
-              {account.name || userName}
+              {account.name}
             </h1>
             <p className='font-ibm-plex-serif font-black text-white'>
               {formatAmount(account.currentBalance)}
@@ -28,7 +29,7 @@ const BankCard = ({account, userName, showBalance
             </div>
             <p className='text-14 font-semibold tracking-[1.1px] text-white'>
             ●●●● ●●●● ●●●● <span className='text-16'>
-                1766
+                {account?.mask}
               </span>
             </p>
 
@@ -60,7 +61,7 @@ const BankCard = ({account, userName, showBalance
         />
       </Link>
 
-      {/**COPY CARD NUMBER */}
+      {showBalance && <Copy title={account.sharaebleId}/>}
       </div>
   )
 }
